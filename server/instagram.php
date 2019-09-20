4a7901cc32c1f768b320ee835a262198e9da47e7
Yuriy Afanasiev <33378584+yuri-afanasiev@users.noreply.github.com>
2019-09-21 01:29:22 +0300

    Add files via upload

--- server/instagram.php
+++ server/instagram.php
@@ -1,15 +1,15 @@
 <?php   
  $url = htmlspecialchars($_GET['url']);
- $json_string = file_get_contents($url. '?__a=1');//https://habrahabr.ru/post/302150/#comment_9634172
+ $json_string = file_get_contents($url. '?__a=1');
  $json = json_decode($json_string, true); 
 
- $video_view_count = $json['graphql']['shortcode_media']['video_view_count'];  
- $video_views = $json['user']['media']['nodes']['0']['video_views'];
- $username = $json['user']['username']; 
- $follows = $json['user']['follows']['count']; 
- $followed_by = $json['user']['followed_by']['count'];  
- $media = $json['user']['media']['count']; 
- $nodes = $json['user']['media']['nodes']['0']['likes']['count']; 
+ $video_view_count = $json['graphql']['user']['edge_owner_to_timeline_media']['edges']['0']['node']['video_view_count'];  
+ $video_views = $json ['graphql']['shortcode_media']['video_view_count'];  
+ $username = $json['graphql']['user']['username']; 
+ $follows = $json['graphql']['user']['edge_followed_by']['count']; 
+ $followed_by = $json['graphql']['user']['edge_follow']['count'];  
+ $media = $json ['graphql']['user']['edge_owner_to_timeline_media']['count']; 
+ $nodes = $json['graphql']['user']['edge_owner_to_timeline_media']['edges']['0']['node']['edge_liked_by']['count'];  
  $media_preview_like = $json['graphql']['shortcode_media']['edge_media_preview_like']['count'];   
 
 $json_data = array ( 'username'=>$username,'follows'=>$follows,'followed_by'=>$followed_by,'media'=>$media,'nodes'=>$nodes,'media_preview_like'=>$media_preview_like,'video_views'=>$video_views,'video_viewcount'=>$video_view_count);
